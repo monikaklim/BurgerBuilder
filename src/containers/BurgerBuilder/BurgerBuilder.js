63d58bfbf801a564e4328f48e00f548c908d9fa5
monikaklim <monyklim@gmail.com>
2020-01-21 16:59:02 +0100

    terminated add ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+
+const INGREDIENT_PRICES = {
+ salad: 0.4,
+ cheese: 0.5,
+ meat: 1.2,
+ bacon: 0.7
+}
+
 class BurgerBuilder extends Component{
 
     state = {
@@ -9,9 +19,30 @@ class BurgerBuilder extends Component{
             bacon:0,
             cheese:0,
             meat:0
-        }
-
+        },
+        totalPrice:4
     }
+
+addIngredientHandler = (type) => {
+const oldCount = this.state.ingredients[type];
+const updatedCounted = oldCount+1;
+const updatedIngredients = {
+    ...this.state.ingredients
+};
+updatedIngredients[type] = updatedCounted;
+const priceAddition = INGREDIENT_PRICES[type]
+const oldPrice = this.state.totalPrice;
+const newPrice = oldPrice + priceAddition;
+this.setState({totalPrice:newPrice, ingredients: updatedIngredients})
+}
+
+removeIngredientHandler = (type) => {
+
+
+}
+
+
+
 
 render(){
 
@@ -20,7 +51,7 @@ render(){
 
             <Burger ingredients = {this.state.ingredients}/>
             <div>
-                Build Control
+                <BuildControls  ingredientAdded = {this.addIngredientHandler}/>
             </div>
 
              </Auxiliary>    
