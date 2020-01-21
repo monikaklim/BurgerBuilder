f268ee0b2ec0aaf92683cc1297dcf2d1f46850c7
monikaklim <monyklim@gmail.com>
2020-01-21 17:46:40 +0100

    order button  added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,8 +20,18 @@ class BurgerBuilder extends Component{
             cheese:0,
             meat:0
         },
-        totalPrice:4
+        totalPrice:4,
+        purchasable:false
     }
+
+updatePurchaseState (ingredients){
+
+const sum = Object.keys(ingredients).map(igKey =>{
+    return ingredients[igKey];}).reduce((sum,el) => {return sum+el;}, 0);
+
+    this.setState({purchasable: sum>0})
+}
+
 
 addIngredientHandler = (type) => {
 const oldCount = this.state.ingredients[type];
@@ -34,11 +44,16 @@ const priceAddition = INGREDIENT_PRICES[type]
 const oldPrice = this.state.totalPrice;
 const newPrice = oldPrice + priceAddition;
 this.setState({totalPrice:newPrice, ingredients: updatedIngredients})
+this.updatePurchaseState(updatedIngredients);
 }
 
 
 removeIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
+    if(oldCount <= 0){
+        return;
+    }
+    else{
     const updatedCounted = oldCount-1;
     const updatedIngredients = {
         ...this.state.ingredients
@@ -48,13 +63,18 @@ removeIngredientHandler = (type) => {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
     this.setState({totalPrice:newPrice, ingredients: updatedIngredients})
-
+    this.updatePurchaseState(updatedIngredients);
+    }
 }
 
 
 
 
 render(){
+const disabledInfo = {...this.state.ingredients};
+for(let key in disabledInfo){
+    disabledInfo[key] = disabledInfo[key] <= 0
+}
 
     return(
             <Auxiliary>
@@ -64,6 +84,9 @@ render(){
                 <BuildControls  
                 ingredientAdded = {this.addIngredientHandler} 
                 ingredientRemoved = {this.removeIngredientHandler}  
+                disabled = {disabledInfo}
+                price = {this.state.totalPrice}
+                purchasable = {this.state.purchasable}
                 />
             </div>
 
