9c771291cb6e9412bd331ec6d108b2be57a92e0b
monikaklim <monyklim@gmail.com>
2020-01-27 11:56:09 +0100

    contact data form terminated

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,8 +3,11 @@ import classes from './Button.module.css'
 
 const button = (props) => (
 
-<button className = {[classes.Button, classes[props.btnType]].join(' ')} 
-onClick = {props.clicked}>
+<button 
+className = {[classes.Button, classes[props.btnType]].join(' ')} 
+onClick = {props.clicked}
+disabled = {props.disabled}
+>
     {props.children}
 </button>
 
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,15 +5,21 @@ import classes from './Input.module.css';
 
 const input = (props) => {
 let inputElement = null;
+const inputClasses = [classes.InputElement];
 
+if(props.invalid && props.shouldValidate && props.touched){
+  inputClasses.push(classes.Invalid);  
+}
 
-switch(props.inputtype){
+switch(props.elementType){
 
-case('input'): inputElement = <input className = {classes.InputElement} {...props}/>;
-break;
-case('textarea'): inputElement = <textarea className = {classes.InputElement}  {...props}/>;
-break;
-default: inputElement = <input  className = {classes.InputElement}  {...props}/>;
+    case('input'): inputElement = <input onChange={props.changed}  className = {inputClasses.join(' ')} {...props.elementConfig}  value = {props.value}/>;
+    break;
+    case('textarea'): inputElement = <textarea onChange= {props.changed} className =  {inputClasses.join(' ')}  {...props.elementConfig}   value = {props.value}/>;
+    break;
+    case('select'): inputElement = <select  onChange= {props.changed} className =  {inputClasses.join(' ')}   value = {props.value}>  {props.elementConfig.options.map(option => (<option key = {option.value}  value = {option.value}>   {option.displayValue} </option> ))}  </select>;
+    break;
+    default: inputElement = <input onChange= {props.changed} className =  {inputClasses.join(' ')}    {...props.elementConfig}    value = {props.value}/> ;
 }
 
 return(
